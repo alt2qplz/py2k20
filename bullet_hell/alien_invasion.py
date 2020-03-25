a33163ac913c58ff0ee15310bf6f19a493cf9205
Alexander Savelichev <alt2qplz@gmail.com>
2020-03-25 23:05:47 +0300

    game_ai_2

--- bullet_hell/alien_invasion.py
+++ bullet_hell/alien_invasion.py
@@ -4,7 +4,6 @@ import pygame
 
 from settings import Settings
 from ship import Ship
-from bullet import Bullet
 
 class AlienInvasion:
 	'''Класс для управления ресурсами и поведением игры'''
@@ -17,24 +16,23 @@ class AlienInvasion:
 		# self.screen = pygame.display.set_mode(
 		# 	(self.settings.screen_width, self.settings.screen_height))
 
+
 		self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 		self.settings.screen_width = self.screen.get_rect().width
 		self.settings.screen_height = self.screen.get_rect().height
 
+
 		pygame.display.set_caption('Alien Invasion')
 
 		self.ship = Ship(self)
-		self.bullets = pygame.sprite.Group()
 
 
 	def run_game(self):
 		'''запуска основного цикла игры'''
 		while True:
 			self._check_events()
-			self.ship.update()
-			self._update_bullets()
 			self._update_screen()
-
+			self.ship.update()
 			
 	def _check_events(self):
 		# Отслеживание событий клавиатуры и мыши
@@ -46,54 +44,39 @@ class AlienInvasion:
 			elif event.type == pygame.KEYUP:
 				self._check_keyup_events(event)
 
-
 	def _check_keydown_events(self, event):
 		'''Реагирует на НАЖАТИЕ клавиш'''
 		if event.key == pygame.K_RIGHT:
 			# Переместить корабль вправо
 			self.ship.moving_right = True
-		elif event.key == pygame.K_LEFT:
+		if event.key == pygame.K_LEFT:
 			# Переместить корабль вправо
 			self.ship.moving_left = True
-		elif event.key == pygame.K_SPACE:
-			self._fire_bullet()
-		elif event.key == pygame.K_q:
+		if event.key == pygame.K_UP:
+			# Переместить корабль вверх
+			self.ship.moving_up = True
+		if event.key == pygame.K_DOWN:
+			# Переместить корабль вниз
+			self.ship.moving_down = True
+		if event.key == pygame.K_q:
 			sys.exit()
-
 
 	def _check_keyup_events(self, event):
 		'''Реагирует на ОТПУСКАНИЕ клавиш'''
 		if event.key == pygame.K_RIGHT:
 			self.ship.moving_right = False
-		elif event.key == pygame.K_LEFT:
+		if event.key == pygame.K_LEFT:
 			self.ship.moving_left = False
-
-
-	def _fire_bullet(self):
-		'''Создание нового снаряда и включение его в группу bullets'''
-		if len(self.bullets) < self.settings.bullets_allowed:
-			new_bullet = Bullet(self)
-			self.bullets.add(new_bullet)
-
-
-	def _update_bullets(self):
-		'''Обновляет позиции снарядов и уничтожает старые снаряды'''
-		# Обновление позиции
-		self.bullets.update()
-
-		# Удаление снарядов, вышедших за край экрана.
-		for bullet in self.bullets.copy():
-			if bullet.rect.bottom <= 0:
-				self.bullets.remove(bullet)
-		print(len(self.bullets))
-
+		if event.key == pygame.K_UP:
+			self.ship.moving_up = False
+		if event.key == pygame.K_DOWN:
+			self.ship.moving_down = False
 
 	def _update_screen(self):
 		# При каждом прохождении цикла перерисовывается экран
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
-		for bullet in self.bullets.sprites():
-			bullet.draw_bullet()
+
 		# Отображение последнего прорисованного экрана
 		pygame.display.flip()
 
